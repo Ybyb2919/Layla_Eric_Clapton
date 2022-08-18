2216048245051e9f863cfcd96487338b79aabc18
dp25 <david.pardo60@gmail.com>
2022-08-18 15:14:06 +0300

    ver #1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 Vers 1 - print("What'll you do when you get lonely
 And nobody's waiting by your side?
 You've been running and hiding much too long
-You know it's just your foolish pride")
+You know it's just your foolish pride yah")
 
 Vers 2 - Tomer
 
