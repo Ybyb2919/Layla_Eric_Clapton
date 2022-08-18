70fede0826e09a4c761e56f5f915fcc102681266
Rc93s <cohenroy93@gmail.com>
2022-08-18 15:34:05 +0300

    feat: log8

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ Vers 4 - Roi
 Layla, you got me on my knees
 Layla, I′m begging darling, please
 Layla, darling, won′t you ease my worried mind?
-
+change the text
 Vers 5 - Yehuda
 Yehuda band is king!
 vers 6 - test
